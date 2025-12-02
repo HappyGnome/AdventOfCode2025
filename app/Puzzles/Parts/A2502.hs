2cdd64e8cd7d9db07c38ab7d3954dd4138459d0a
HappyGnome <46838482+HappyGnome@users.noreply.github.com>
2025-12-02 06:05:11 +0000

    Day 2 rough

--- app/Puzzles/Parts/A2502.hs
+++ app/Puzzles/Parts/A2502.hs
@@ -51,7 +51,7 @@ exec1 :: String -> String -> String -> IO()
 exec1 inpPathBase inpPath0 inpPath1 = do
     let 
         --inpPath2 = inpPathBase ++ "Test2.txt"
-{-@@1-}inpPath = inpPath0         -- Choose Test or Input here 
+{-@@1-}inpPath = inpPath1         -- Choose Test or Input here 
 
     ls <- IOH.getFileLines inpPath
     printSoln "Part 1" inpPath $ solve1 $ parseLines ls
@@ -62,7 +62,7 @@ exec2 :: String -> String -> String -> IO()
 exec2 inpPathBase inpPath0 inpPath1 = do
     let 
         --inpPath2 = inpPathBase ++ "Test2.txt"
- {-@@2-}inpPath = inpPath0         -- Choose Test or Input here 
+ {-@@2-}inpPath = inpPath1         -- Choose Test or Input here 
 
     ls <- IOH.getFileLines inpPath
     printSoln "Part 2" inpPath $ solve2 $ parseLines ls
@@ -84,25 +84,76 @@ execDebug inpPathBase inpPath0 inpPath1 = do
 type ParseLineResult = Int --- Best to replace ParseLineResult with the actual type, if it's simple enough.
                             -- Defining this here just to allow us to warm up the compilet on the blank file
 
-parseLines :: [String] -> ParseLineResult
+parseLines :: [String] -> [(Int,Int)]
 parseLines ls = 
-    0
+   let 
+        pr cs = (\css -> (readInt $ head css, readInt $ css !! 1)) $ splitOnPred (=='-') cs
+        ln = splitOnPred (==',')
+    in
+        map pr $ ln $ concat ls
 
 --------------------------------------------------------------------------------------------
 -- Solver
 
-solve1 :: ParseLineResult ->  Maybe Int
+solve1 :: [(Int,Int)] ->  Maybe Int
 solve1 plr = -- @@1
-    Nothing
+    let
 
-solve2 :: ParseLineResult -> Maybe Int
+    in
+        Just $ sum $ map invInRange plr
+        
+                
+
+solve2 :: [(Int,Int)] -> Maybe Int
 solve2 plr = -- @@2
-    Nothing
+    Just $ sum $ map invInRange2 plr
 
-solveDebug :: ParseLineResult ->  IO()
+solveDebug :: [(Int,Int)] ->  IO()
 solveDebug plr = do
     return ()
 --------------------------------------------------------------------------------------------
 -- Business
 
+-- splitDig x = splitDig' (x,0,1)
+-- splitDig' (x,y,z)
+--     | x <= y = (x,y)
+--     | otherwise = splitDig' (x `div` 10, (x `mod` 10) * z + y, z * 10)
+-- 
+-- isInvalid x = z == y 
+--     where
+--         (z,y) = splitDig x
+--
 
+isInvalid x = 
+    let
+        (st,nd) = splitAt (length cs `div` 2) $ cs
+            where
+                cs = show x
+    in
+        st == nd
+
+isInvalid2 x = 
+    let
+
+        testStr cs  = testStr' cs 1
+
+        testStr' cs n
+            | 2*n > length cs = False
+            | all ((==) $ head $ ptsc) ptsc = True
+            | otherwise = testStr' cs (n+1)
+            where
+
+                ptsc = pts cs
+
+                pts [] = []
+                pts ds = st : pts nd 
+                    where
+                        (st, nd) = splitAt n ds
+
+                
+    in
+        testStr $ show x
+
+invInRange (x,y) = sum $ filter isInvalid [x..y]
+
+invInRange2 (x,y) = sum $ filter isInvalid2 [x..y]
