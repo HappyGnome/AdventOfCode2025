980922ecdcd924f257828d122867b168b416175c
HappyGnome <46838482+HappyGnome@users.noreply.github.com>
2025-12-04 06:25:53 +0000

    Day 4 rough

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,11 @@ of style and logic. This is a learning project for the author.
 
 module Main where
 
-import A2503 -- <<Current<< 
+import A2504 -- <<Current<< 
 
 main :: IO ()
 main = do
-    A2503.exec -- <<Current<< 
+    A2504.exec -- <<Current<< 
 
 
 
--- app/Puzzles/Parts/A2504.hs
+++ app/Puzzles/Parts/A2504.hs
@@ -12,7 +12,7 @@ of style and logic. This is a learning project for the author, and has been prep
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
-module A25xx where -- <<Current<<
+module A2504 where
 
 import Text.Read
 import Data.Maybe
@@ -35,7 +35,7 @@ import ArithEx
 
 
 problemNumber :: String
-problemNumber = "A25xx" -- <<Current<<
+problemNumber = "A2504"
 
 -- Entry point. Control which steps to run here
 exec :: IO()
@@ -71,7 +71,7 @@ exec1 :: String -> String -> String -> IO()
 exec1 inpPathBase inpPath0 inpPath1 = do
     let 
         --inpPath2 = inpPathBase ++ "Test2.txt"
-{-@@-}inpPath = inpPath0         -- Choose Test or Input here 
+{-@@-}inpPath = inpPath1         -- Choose Test or Input here 
 
     readParseSolve "Part 1" inpPath parseLines solve1
 
@@ -81,7 +81,7 @@ exec2 :: String -> String -> String -> IO()
 exec2 inpPathBase inpPath0 inpPath1 = do
     let 
         --inpPath2 = inpPathBase ++ "Test2.txt"
- {-@@-}inpPath = inpPath0         -- Choose Test or Input here 
+ {-@@-}inpPath = inpPath1         -- Choose Test or Input here 
 
     readParseSolve "Part 2" inpPath parseLines solve2
 
@@ -102,24 +102,62 @@ execDebug inpPathBase inpPath0 inpPath1 = do
 type ParseLineResult = Int --- Best to replace ParseLineResult with the actual type, if it's simple enough.
                             -- Defining this here just to allow us to warm up the compilet on the blank file
 
-parseLines :: [String] -> ParseLineResult
-parseLines ls = 
-    0
+parseLines :: [String] -> Map.Map (V2 Int) Bool
+parseLines = 
+    IOH.loadJust2D (\x -> if x == '@' then Just True else Nothing)
 
 --------------------------------------------------------------------------------------------
 -- Solver
 
-solve1 :: ParseLineResult ->  Maybe Int
+solve1 :: Map.Map (V2 Int) Bool ->  Maybe Int
 solve1 plr = -- @@
-    Nothing
+    let
+        allAdj = [Up,Dn,Lt,Rt,Ul,Ur,Dl,Dr]
+        nbrs p = filter id $ mapMaybe (\dir -> plr Map.!? stepInDir8 dir p) allAdj
+        validCell p _ = length (nbrs p) < 4
+    in
+      Just $ length $ Map.toList $ Map.filter id $ Map.mapWithKey validCell plr
 
-solve2 :: ParseLineResult -> Maybe Int
+
+solve2 :: Map.Map (V2 Int) Bool -> Maybe Int
 solve2 plr = -- @@
-    Nothing
+    let
+        allAdj = [Up,Dn,Lt,Rt,Ul,Ur,Dl,Dr]
+        nbrs plr' p = filter id $ mapMaybe (\dir -> plr' Map.!? stepInDir8 dir p) allAdj
+        validCell plr' p _ = length (nbrs plr' p) < 4
 
-solveDebug :: ParseLineResult ->  IO()
+        nextMap cnt plr' 
+            | new > 0 = nextMap (cnt + new) $  Map.map not $ Map.filter not candidate 
+            | otherwise = cnt
+            where
+                candidate = Map.mapWithKey (validCell plr') plr'
+                new = length $ Map.filter id candidate
+
+    in
+      Just $ nextMap 0 plr
+
+
+solveDebug :: Map.Map (V2 Int) Bool ->  IO()
 solveDebug plr = do
-    return ()
+    let
+        allAdj = [Up,Dn,Lt,Rt,Ul,Ur,Dl,Dr]
+        nbrs plr' p = filter id $ mapMaybe (\dir -> plr' Map.!? stepInDir8 dir p) allAdj
+        validCell plr' p _ = length (nbrs plr' p) < 4
+
+        nextMap cnt plr' 
+            | new > 0 = (print () >> rende candidate)  >> (rende $ Map.filter not candidate) >> (nextMap (cnt + new) $ Map.map not $ Map.filter not candidate )
+            | otherwise = print cnt
+            where
+                candidate = Map.mapWithKey (validCell plr') plr'
+                new = length $ Map.filter id candidate
+
+        rend (Just True) = '@'
+        rend (Just False) = '!'
+        rend _ = '.'
+
+        rende = IOH.renderGrid 10 10 rend
+        
+    nextMap 0 plr
 --------------------------------------------------------------------------------------------
 -- Business
 
